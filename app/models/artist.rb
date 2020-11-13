62d6d845b6fa5c978150982d385dadeea119c458
Technicallyaustin <Technicallyaustin@gmail.com>
2020-11-12 19:23:20 -0800

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,17 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def find_or_create_by(artist_info)
+        if Artist.find(artist_info)
+            Artist.find(artist_info)
+        else
+            Artist.create(artist_info)
+        end
+    end
+
+    def song_count
+        self.songs.count
+    end
+    
+    
 end
